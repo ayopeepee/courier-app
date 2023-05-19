76ac24522ece2b4f83cb78e3bc9b05ddc2ec85f7
ayopeepee <topdenmax@email.ru>
2023-05-19 17:45:26 +0300

    Add Firebase auth

---
+++ app/src/main/java/com/example/delivery/SignUpActivity.kt
@@ -0,0 +1,41 @@
+package com.example.delivery
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.delivery.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.buttonSignup.setOnClickListener {
+            val email = binding.textInputEmail.text.toString()
+            val password = binding.textInputPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, SignInActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Fields are empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
